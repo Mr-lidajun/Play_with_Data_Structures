ce0157d4c32d2f80aa3dffe9df1b7bd26c341dff
lidajun828 <!QAZ2wsx123456>
2018-08-01 18:21:22 +0800

    Chapter 03 section 03.栈的另一个应用：括号匹配（LeetCode第20题：有效的括号）

---
+++ 03-Stacks-and-Queues/03-A-Stack-Problem-in-Leetcode/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        boolean valid = solution.isValid("([{}])");
+        System.out.println(valid);
+    }
+}
---
+++ 03-Stacks-and-Queues/03-A-Stack-Problem-in-Leetcode/src/Solution.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                Character topChar = stack.pop();
+                if (c == ')' && topChar != '(') {
+                    return false;
+                } else if (c == ']' && topChar != '[') {
+                    return false;
+                } else if (c == '}' && topChar != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
